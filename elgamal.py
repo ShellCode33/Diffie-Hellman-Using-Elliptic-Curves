1b7ec96ddfb8b32e5d44862104759921671b8895
Chopanaweur <chopanaweur54@protonmail.com>
2018-03-23 13:25:13 +0100

    update Elgamal

--- elgamal.py
+++ elgamal.py
@@ -2,24 +2,24 @@ from ellipticcurve import EllipticCurve
 
 class ElGamal(EllipticCurve):
 
-    def __init__(self):
-
+    def __init__(self, msg):
+        #TODO : convert msg to msg point
         pass
 
-    def __initElGamalParameters(self, M):
-        self.private_Key_A = randint(1, self.find_point_order(M)-1)#trouver le poitn P au préalable
-        self.public_Key_A = self.fast_exp(self.private_Key_A, M)#Pareil ajouter le point P
-        self.message = self.fast_exp(randint(1, self.find_point_order(M)-1), M)#M doit appartenir au groupe engendré par P
+    def __initElGamalParameters(self, msg_point):
+        self.private_key = randint(1, self.point_order(msg_point)-1)#trouver le poitn P au préalable
+        self.private_key = self.fast_exp(self.private_key, msg_point)#Pareil ajouter le point P
+        self.message = self.fast_exp(randint(1, self.point_order(msg_poin)-1), msg_point)#M doit appartenir au groupe engendré par P
 
-    def encrypt(self, M):
-        self.private_Key_B = randint(1, self.find_point_order())
-        self.c1 = self.fast_exp(self.private_Key_B, )#Pareil le point P
-        self.c2 = self.addPoints(M, self.fast_exp(self.private_Key_B, self.public_Key_A))
+    def encrypt(self, msg_point):
+        self.coef_k = randint(1, self.point_order(msg_point))
+        self.c1 = self.fast_exp(self.coef_k, )#Pareil le point P
+        self.c2 = self.addPoints(msg_point, self.fast_exp(self.coef_k, self.private_key))
         self.encrypted_message = self.addPoints(self.c1, self.c2)
         return self.encrypted_message
 
-    def decrypt(self, M):
-        self.decrypt_step_one = self.fast_exp(self.private_Key_B, self.public_Key_A)
+    def decrypt(self, msg_point):
+        self.decrypt_step_one = self.fast_exp(self.coef_k, self.private_key)
         self.decrypted_message = self.addPoints(self.c1, self.decrypt_step_one)
         return self.decrypted_message
 
@@ -27,6 +27,6 @@ class ElGamal(EllipticCurve):
         super().askCurveToUse()
         self.__initElGamalParameters()
 
-    def completeElGamal(self, M):
+    def completeElGamal(self, msg_point):
         self.askCurveToUse()
         self.

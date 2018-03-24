dd7c2a7699925971b893c22b5783a9bd0fb0cb1a
ShellCode33 <clement.fleury33@gmail.com>
2018-03-24 21:19:05 +0100

    Comment MathUtils class

--- core/mathutils.py
+++ core/mathutils.py
@@ -5,6 +5,11 @@ class MathUtils(object):
 
     @staticmethod
     def egcd(a, b):
+        """
+        Extended Euclidean algorithm
+        :param a
+        :param b
+        """
         if a == 0:
             return b, 0, 1
         else:
@@ -13,6 +18,11 @@ class MathUtils(object):
 
     @staticmethod
     def modinv(a, m):
+        """
+        Modular inverse calculation
+        :param a: the number we want to inverse
+        :param m: the modulus
+        """
         g, x, y = MathUtils.egcd(a, m)
         if g != 1:
             print("a: " + str(a) + " ; m: " + str(m) + " ; g: " + str(g))
@@ -22,10 +32,20 @@ class MathUtils(object):
 
     @staticmethod
     def symetric(point):
+        """
+        :param point
+        :return: Symetric of the point
+        """
         return point[0], -point[1]
 
     @staticmethod
     def numberToBase(num, base):
+        """
+        Convert a number into digits list of specified base
+        :param num
+        :param base
+        :return: a list of digits
+        """
         if num == 0:
             return [0]
         digits = []
@@ -36,6 +56,12 @@ class MathUtils(object):
 
     @staticmethod
     def baseToNumber(digits, base):
+        """
+        Convert a list of digits into a number of specified base
+        :param digits
+        :param base
+        :return: a number
+        """
         if len(digits) == 1:
             return digits[0]
         else:

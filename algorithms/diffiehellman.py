9b652e70f51c107c11fae1ed35da40a277c5bb0a
ShellCode33 <clement.fleury33@gmail.com>
2018-03-24 20:48:38 +0100

    Comment Diffie Hellman class

--- algorithms/diffiehellman.py
+++ algorithms/diffiehellman.py
@@ -13,17 +13,30 @@ class DiffieHellman(EllipticCurve):
         self.__initDiffieHellmanParameters()
 
     def __initDiffieHellmanParameters(self):
+        """
+        This method creates the Diffie Hellman parameters
+        """
         self.dh_random = randint(0, self.point_order - 1)
         self.dh_param = self.fast_exp(self.dh_random, self.init_point)
 
     def getParameterToSend(self):
+        """
+        Getter
+        :return: Diffie Hellman parameter to be sent over the network
+        """
         return self.dh_param
 
     def askCurveToUse(self):
+        """
+        Overriding EllipticCurve's method in order to init ElGamal parameters after
+        """
         super(DiffieHellman, self).askCurveToUse()
         self.__initDiffieHellmanParameters()
 
-    # Permet d'utiliser le paramètre envoyé par la personne avec qui on souhaite échanger afin de générer la clé secrète
     def completeDiffieHellmanExchange(self, B):
+        """
+        Completes the DH exchange and calculate the secret key thanks to the parameter B
+        :param B: sent over the network by the person you're communicating with
+        """
         secret_key = self.fast_exp(self.dh_random, B)
         print("sha1(secret_key) = " + hashlib.sha1(pickle.dumps(secret_key)).hexdigest())
